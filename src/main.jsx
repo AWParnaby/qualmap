d8c7d4b35af3f9772c6d34953aaf30bab506a9f9
Adam <contact@adamparnaby.com>
2025-02-16 17:15:37 +0000

    Made the whole app more modular and extensible without changing functionality.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import MapWithWordCloud from './MapWithWordCloud.jsx'
+import { MapDataProvider } from './contexts/MapDataContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MapWithWordCloud />
+    <MapDataProvider>
+      <MapWithWordCloud />
+    </MapDataProvider>
   </StrictMode>,
 )
